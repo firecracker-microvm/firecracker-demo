c22499567b63b4edd85e19ca9b0e9fa398b3300b
Alexandra Iordache <aghecen@amazon.com>
2019-11-12 14:51:33 +0200

    suppress stdout&stderr
    Signed-off-by: Alexandra Iordache <aghecen@amazon.com>

--- start-firecracker.sh
+++ start-firecracker.sh
@@ -54,7 +54,7 @@ KERNEL_BOOT_ARGS="${KERNEL_BOOT_ARGS} ip=${FC_IP}::${TAP_IP}:${MASK_LONG}::eth0:
 # Start Firecracker API server
 rm -f "$API_SOCKET"
 
-./firecracker --api-sock "$API_SOCKET" --id "${SB_ID}" &
+./firecracker --api-sock "$API_SOCKET" --id "${SB_ID}" &>/dev/null &
 
 sleep 0.015s
 
